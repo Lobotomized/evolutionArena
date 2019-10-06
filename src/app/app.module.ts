b73051d7383834d2ecda1c687437df482f390aa3
unknown <ivan_dzholev@abv.bg>
2019-10-06 22:02:05 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
